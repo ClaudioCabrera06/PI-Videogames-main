9e89c0196e4709276d8cd041d89e15cb6ed69491
ClaudioCabrera06 <113956971+ClaudioCabrera06@users.noreply.github.com>
2023-04-13 17:49:52 -0300

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -6,7 +6,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://postgres:43911584@localhost:5432/videogames`, {
+const sequelize = new Sequelize(``, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
